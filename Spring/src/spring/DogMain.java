3a206d1d3e64bfc15f1d9377afd5a0ae9e5a0ebd
Myroslav <mirik12mirik@gmail.com>
2018-11-03 14:03:42 +0100

    1

---
+++ Spring/src/spring/DogMain.java
@@ -0,0 +1,24 @@
+package spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class DogMain {
+
+	public static void main(String[] args) {
+		
+		try {
+			//ApplicationContext context= new ClassPathXmlApplicationContext("C:\\Users\\app-context.xml");
+			ApplicationContext context= new FileSystemXmlApplicationContext("C:\\Users\\ой\\eclipse-workspace\\Spring\\bin\\app-context.xml");
+		Dog dog=(Dog) context.getBean("dogBean");
+		//Owner owner=(Owner) context.getBean("ownerBean");
+		
+		System.out.println(dog);
+		//System.out.println(owner);
+		} catch (BeansException e) {
+			e.printStackTrace();
+		}
+}
+}
